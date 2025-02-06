141e0dc721cafa6ccbc04986592af767b77b2b96
suppHorter <max@horter.de>
2025-02-06 23:03:04 +0000

    cleanup

--- src/js/graph/abstract.js
+++ src/js/graph/abstract.js
@@ -1,12 +1,12 @@
 class Graph {
-  #width = 600;
-  #height = 400;
   #context;
   #svgId;
-  #dataUrl;
+  #dataUrl = 'output.json';
   #groupKeys;
-  #margin = { top: 20, right: 30, bottom: 40, left: 40 };
   #filter;
+  #width = 600;
+  #height = 400;
+  #margin = { top: 20, right: 30, bottom: 40, left: 40 };
 
   constructor(config) {
     if (!config.svgId) {
@@ -15,11 +15,9 @@ class Graph {
     }
     this.#svgId = config.svgId;
 
-    let dataUrl = "output.json";
     if (config.dataUrl) {
-      dataUrl = config.dataUrl;
+      this.#dataUrl = config.dataUrl;
     }
-    this.#dataUrl = dataUrl;
 
     if (config.filter) {
       this.#filter = config.filter;
@@ -44,19 +42,16 @@ class Graph {
     this.#height = this.#height - this.#margin.top - this.#margin.bottom;
 
     this.#context = d3.select(`#${this.#svgId}`)
-      .attr("width", this.#width + this.#margin.left + this.#margin.right)
-      .attr("height", this.#height + this.#margin.top + this.#margin.bottom)
-      .append("g")
-      .attr("transform", `translate(${this.#margin.left},${this.#margin.top})`);
+      .attr('width', this.#width + this.#margin.left + this.#margin.right)
+      .attr('height', this.#height + this.#margin.top + this.#margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${this.#margin.left},${this.#margin.top})`);
 
-    // let groupKeys = this.#groupKeys;
     d3.json(this.#dataUrl).then(data => {
       if (this.#filter) {
         data = this.#filter(data);
       }
-      // if (groupKeys) {
-      //   data = d3.group(data, d => d[groupKeys[0]])
-      // }
+      data.sort((a, b) => new Date(a.date) - new Date(b.date));
       callback(this.#context, data);
     }).catch(error => {
       console.error('Error fetching the data:', error);
@@ -64,11 +59,11 @@ class Graph {
   }
 
   drawAxis(xAxis, yAxis) {
-    this.#context.append("g")
-      .attr("transform", `translate(0,${this.#height})`)
+    this.#context.append('g')
+      .attr('transform', `translate(0,${this.#height})`)
       .call(d3.axisBottom(xAxis));
 
-    this.#context.append("g")
+    this.#context.append('g')
       .call(d3.axisLeft(yAxis));
   }
 
--- src/js/graph/timeline.js
+++ src/js/graph/timeline.js
@@ -1,5 +1,5 @@
 class TimelineGraph extends Graph {
-  #dateFormat = "%d.%m.%Y";
+  #dateFormat = '%d.%m.%Y';
   constructor(config) {
     super(config);
   }
@@ -17,7 +17,7 @@ class TimelineGraph extends Graph {
           groupedData = d3.group(data, d => d[key]);
         }
       } else {
-        groupedData = new Map([["all", data]]);
+        groupedData = new Map([['all', data]]);
       }
 
       let xAxis = d3.scaleTime()
@@ -39,66 +39,65 @@ class TimelineGraph extends Graph {
         .curve(d3.curveMonotoneX);
 
       groupedData.forEach((values, key) => {
-        context.append("path")
+        context.append('path')
           .datum(values)
-          .attr("class", "line")
-          .attr("d", line)
-          .style("fill", "none")
-          .style("stroke", color(key))
-          .style("stroke-width", 2);
+          .attr('class', 'line')
+          .attr('d', line)
+          .style('fill', 'none')
+          .style('stroke', color(key))
+          .style('stroke-width', 2);
 
-        context.selectAll("dot")
+        context.selectAll('dot')
           .data(values)
-          .enter().append("circle")
-          .attr("class", "dot")
-          .attr("cx", d => xAxis(new Date(d.date)))
-          .attr("cy", d => yAxis(d.value))
-          .attr("r", 3)
-          .style("fill", color(key))
-          .on("mouseover", function(event, d) {
-            d3.select(this).transition().duration(100).attr("r", 6);
-            context.append("text")
-              .attr("class", "tooltip-date")
-              .attr("x", 10)
-              .attr("y", height - 15)
+          .enter().append('circle')
+          .attr('class', 'dot')
+          .attr('cx', d => xAxis(new Date(d.date)))
+          .attr('cy', d => yAxis(d.value))
+          .attr('r', 3)
+          .style('fill', color(key))
+          .on('mouseover', function(event, d) {
+            d3.select(this).transition().duration(100).attr('r', 6);
+            context.append('text')
+              .attr('class', 'tooltip-date')
+              .attr('x', 10)
+              .attr('y', height - 15)
               .text(formatDate(new Date(d.date)))
-              .style("font-size", "20px")
-              .style("font-weight", "bold")
-            context.append("text")
-              .attr("class", "tooltip-value")
-              .attr("x", 10)
-              .attr("y", height - 35)
+              .style('font-size', '20px')
+              .style('font-weight', 'bold')
+            context.append('text')
+              .attr('class', 'tooltip-value')
+              .attr('x', 10)
+              .attr('y', height - 35)
               .text(d.value)
-              .style("font-size", "24px")
-              .style("font-weight", "bold")
+              .style('font-size', '24px')
+              .style('font-weight', 'bold')
           })
-          .on("mouseout", function(event, d) {
-            d3.select(this).transition().duration(100).attr("r", 3);
-            context.select(".tooltip-date").remove();
-            context.select(".tooltip-value").remove();
+          .on('mouseout', function(event, d) {
+            d3.select(this).transition().duration(100).attr('r', 3);
+            context.select('.tooltip-date').remove();
+            context.select('.tooltip-value').remove();
           });
       });
 
-      // Legende hinzufügen und am unteren Rand platzieren
-      const legend = context.append("g")
-        .attr("class", "legend")
-        .attr("transform", `translate(0, ${height + 20})`);
+      const legend = context.append('g')
+        .attr('class', 'legend')
+        .attr('transform', `translate(0, ${height + 20})`);
       
       let i = -1;
       groupedData.forEach((values, key, index) => {
         i++;
-        const legendRow = legend.append("g")
-          .attr("transform", `translate(${i * 30}, 0)`);
+        const legendRow = legend.append('g')
+          .attr('transform', `translate(${i * 30}, 0)`);
 
-        legendRow.append("rect")
-          .attr("width", 10)
-          .attr("height", 10)
-          .attr("fill", color(key));
+        legendRow.append('rect')
+          .attr('width', 10)
+          .attr('height', 10)
+          .attr('fill', color(key));
 
-        legendRow.append("text")
-          .attr("x", 15) // Abstand zwischen Rechteck und Text
-          .attr("y", 10)
-          .style("text-transform", "capitalize")
+        legendRow.append('text')
+          .attr('x', 15)
+          .attr('y', 10)
+          .style('text-transform', 'capitalize')
           .text(key);
       });
     });
